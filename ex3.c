e92eb056c4ef9c6db0a57dec2f497a6d53800d17
yarin181 <yarinsason@gmail.com>
2023-05-19 15:22:40 +0300

    structure plan

--- ex3.c
+++ ex3.c
@@ -1,70 +1,142 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-
-typedef struct {
-    int id;
-    int type;
-    int index;
-} News;
-
-typedef struct {
-    int capacity;
-    sem_t mutex;
-    sem_t empty;
-    sem_t full;
-    News * NewsList;
-} BoundedBuffer;
-
-typedef struct {
-    int Capacity;
-    sem_t mutex;
-    sem_t full;
-    News * NewsList;
-}UnBoundedBuffer;
-
-typedef struct{
-    BoundedBuffer * boundedBuffer;
-    int id;
-    int numberOfProducts;
-}Producer;
-
-typedef struct {
-    BoundedBuffer ** BoundedBuffersList;
-    UnBoundedBuffer * unBoundedBufferList[3];
-
-}Dispatcher;
-
-typedef struct {
-    UnBoundedBuffer * unBoundedBuffer;
-    BoundedBuffer * screenManagerBuffer;
-}CoEditors;
-
-typedef struct {
-    BoundedBuffer boundedBuffer;
-}ScreenManager;
+#include "ex3.h"
 
 ///constructor of News (id,type,index)
 
-///constructor of producer (id , numberOfProducts,size)
-
 ///construct of BoundedBuffer (size) return BoundedBuffer*
+BoundedBuffer * createBoundedBuffer(int queueSize){
+    BoundedBuffer * newBoundedBuffer = malloc(sizeof(BoundedBuffer));
+    newBoundedBuffer->NewsList = malloc(sizeof (char *)*queueSize);
+    sem_init(&newBoundedBuffer->empty,0,queueSize);
+    sem_init(&newBoundedBuffer->full,0,0);
+    sem_init(&newBoundedBuffer->mutex,0,1);
+    newBoundedBuffer->firstIndex = 0;
+    newBoundedBuffer->lastIndex = 0;
+    newBoundedBuffer->size = queueSize;
+    return newBoundedBuffer;
+}
 
 ///construct of UnBoundedBuffer (size) return UnBoundedBuffer*
+UnBoundedBuffer * createUnBoundedBuffer(){
+    UnBoundedBuffer * newUnBoundedBuffer = malloc(sizeof(UnBoundedBuffer));
+    newUnBoundedBuffer->currentSize = 20;
+    newUnBoundedBuffer->NewsList = malloc(sizeof (char *) * newUnBoundedBuffer->currentSize);
+    sem_init(&newUnBoundedBuffer->full,0,0);
+    sem_init(&newUnBoundedBuffer->mutex,0,1);
+    newUnBoundedBuffer->firstIndex = 0;
+    newUnBoundedBuffer->lastIndex = 0;
+    return newUnBoundedBuffer;
+}
 
-///constructor of the dispatcher , init his 3 queues with the co-editors.
+///constructor of producer (id , numberOfProducts,size) (create a bounded buffer)
+Producer *  createProducer(int id, int numberOfProducts, int queueSize){
+    Producer * newProducer = malloc(sizeof (Producer));
+    newProducer->boundedBuffer = createBoundedBuffer(queueSize);
+    newProducer->id = id;
+    newProducer->numberOfProducts = numberOfProducts;
+    return newProducer;
+}
 
-///constructor of the Screen Manager;
+///constructor of the dispatcher. (probably will not needed cause i maje it on the stack.)
 
-/// function - add a bounded buffer to the Dispatcher Unbounded Buffer queue.
+///constructor for the CoEditors (each create unboundedList)
+void initCoEditor(CoEditor *coEditor,BoundedBuffer * screenManagerBuffer){
+    coEditor->screenManagerBuffer = screenManagerBuffer;
+    coEditor->unBoundedBuffer = createUnBoundedBuffer();
+}
+
+///constructor of the Screen Manager (create bounded list)
+void initScreenManager(ScreenManager * screenManager, int queueSize){
+    screenManager->boundedBuffer = createBoundedBuffer(queueSize);
+}
+
+/// function - add a bounded buffer to the Dispatcher Unbounded Buffer queue.(probably will not needed cause i add them on the main.)
+
+///insertItem to a bounded buffer (critical section)
+void insertItemBounded(BoundedBuffer * boundedBuffer,char *news){
+    boundedBuffer->lastIndex =  (boundedBuffer->lastIndex + 1) % boundedBuffer->size;
+    boundedBuffer->NewsList[boundedBuffer->lastIndex] = news;
+}
 
 ///function - add a news to a bounded buffer.
+void insertBounded(BoundedBuffer * boundedBuffer,char * news){
+    sem_wait(&boundedBuffer->empty);
+    sem_wait(&boundedBuffer->mutex);
+    insertItemBounded(boundedBuffer,news);
+    sem_post(&boundedBuffer->mutex);
+    sem_post(&boundedBuffer->full);
 
+}
+
+/// remove an item from the buffer (critical section)
+char * removeItemBounded(BoundedBuffer * boundedBuffer){
+    char * itemPtr = boundedBuffer->NewsList[boundedBuffer->firstIndex];
+    boundedBuffer->NewsList[boundedBuffer->firstIndex] =NULL;
+    boundedBuffer->firstIndex  = (boundedBuffer->firstIndex + 1) % boundedBuffer->size;
+    return itemPtr;
+
+}
 ///function - remove a news from a bounded buffer.
+char * removeBounded(BoundedBuffer * boundedBuffer){
+    char * itemPtr = NULL;
+    sem_wait(&boundedBuffer->full);
+    sem_wait(&boundedBuffer->mutex);
+    itemPtr = removeItemBounded(boundedBuffer);
+    sem_post(&boundedBuffer->mutex);
+    sem_post(&boundedBuffer->empty);
+    return itemPtr;
+}
+
+///increase the newsList of unBounded buffer
+void increaseUnBoundedSize(UnBoundedBuffer *unBoundedBuffer){
+    unBoundedBuffer->currentSize *= 2;
+    char ** newBuffer = malloc(sizeof (char *) * unBoundedBuffer->currentSize);
+    int currentIndex = 0;
+    while (unBoundedBuffer->firstIndex != unBoundedBuffer->lastIndex){
+        newBuffer[currentIndex] = unBoundedBuffer->NewsList[unBoundedBuffer->firstIndex];
+        currentIndex ++;
+        unBoundedBuffer->firstIndex++;
+    }
+    unBoundedBuffer->lastIndex = currentIndex - 1;
+    unBoundedBuffer->firstIndex = 0;
+    free(unBoundedBuffer->NewsList);
+    unBoundedBuffer->NewsList =newBuffer;
+}
+
+/// insert an item from the buffer (critical section)
+void insertItemUnBounded(UnBoundedBuffer * unBoundedBuffer,char *news){
+    if ((unBoundedBuffer->lastIndex + 1) % unBoundedBuffer->currentSize == unBoundedBuffer->firstIndex % unBoundedBuffer->currentSize){
+        increaseUnBoundedSize(unBoundedBuffer);
+    }
+    unBoundedBuffer->lastIndex =  (unBoundedBuffer->lastIndex + 1) % unBoundedBuffer->currentSize;
+    unBoundedBuffer->NewsList[unBoundedBuffer->lastIndex] = news;
+}
 
 ///function - add a news to a unBounded buffer.
+void insertUnBounded(UnBoundedBuffer * unBoundedBuffer,char * news){
+    sem_wait(&unBoundedBuffer->mutex);
+    insertItemUnBounded(unBoundedBuffer,news);
+    sem_post(&unBoundedBuffer->mutex);
+    sem_post(&unBoundedBuffer->full);
+
+}
+
+///remove an item from the buffer (critical section).
+char * removeItemUnBounded(UnBoundedBuffer * unBoundedBuffer){
+    char * itemPtr = unBoundedBuffer->NewsList[unBoundedBuffer->firstIndex];
+    unBoundedBuffer->NewsList[unBoundedBuffer->firstIndex] =NULL;
+    unBoundedBuffer->firstIndex  = (unBoundedBuffer->firstIndex + 1) % unBoundedBuffer->currentSize;
+    return itemPtr;
+}
 
 ///function - remove a news from a unBounded buffer.
+char * removeUnBounded(UnBoundedBuffer * unBoundedBuffer){
+    char * itemPtr = NULL;
+    sem_wait(&unBoundedBuffer->full);
+    sem_wait(&unBoundedBuffer->mutex);
+    itemPtr = removeItemUnBounded(unBoundedBuffer);
+    sem_post(&unBoundedBuffer->mutex);
+    return itemPtr;
+}
 
 ///function for the screen manager that takes a News from his queue and print it to the screen.
 
@@ -84,7 +156,12 @@ int main(int argc,char * argv[]) {
     ///create the dispatcher.
     ///create the screen manager.
     ///attached the producer queues and the co Editors queues to the dispatcher.
-
+    ///attached the 3 coEditors to the screen managerUnboundedList.
+    ///activate all the producers( each in separate thread)
+    ///activate the dispatcher (in a thread)
+    ///activate the three co Editors.( in a thread)
+    ///activate the Screen Manager. (in a thread)
+    ///wait for the Screen manger thread to finish.
 
     return 0;
 }
--- ex3.h
+++ ex3.h
@@ -1,6 +1,57 @@
 #ifndef OP_ASS3_EX3_H
 #define OP_ASS3_EX3_H
 
+#define CO_EDITOR_NUM 3
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
+//typedef struct {
+//    int id;
+//    int type;
+//    int index;
+//} News;
+typedef struct {
+    int firstIndex;
+    int lastIndex;
+    int size;
+    sem_t mutex;
+    sem_t empty;
+    sem_t full;
+    char ** NewsList;
+} BoundedBuffer;
+
+typedef struct {
+    int firstIndex;
+    int lastIndex;
+    int currentSize;
+    sem_t mutex;
+    sem_t full;
+    char ** NewsList;
+}UnBoundedBuffer;
+
+typedef struct{
+    BoundedBuffer * boundedBuffer;
+    int id;
+    int numberOfProducts;
+}Producer;
+
+typedef struct {
+    BoundedBuffer ** BoundedBuffersList;
+    UnBoundedBuffer * unBoundedBufferList[CO_EDITOR_NUM];
+
+}Dispatcher;
+
+typedef struct {
+    UnBoundedBuffer * unBoundedBuffer;
+    BoundedBuffer * screenManagerBuffer;
+}CoEditor;
+
+typedef struct {
+    BoundedBuffer *boundedBuffer;
+}ScreenManager;
+
 
 
 
